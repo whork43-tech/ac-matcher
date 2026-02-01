369d39f013b52a5525c0e77d53c5a4aa76b3d5ae
whork43-tech <whork43@gmail.com>
2026-02-01 20:29:45 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def landing(request: Request):
+    return templates.TemplateResponse("landing.html", {"request": request})
